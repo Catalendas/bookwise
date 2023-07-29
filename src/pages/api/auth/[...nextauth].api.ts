a544fbc50409527674a742fa20549007d3d682d4
marcos <musiccar@outlook.com>
2023-07-28 21:27:05 -0300

    Diversos

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -4,7 +4,7 @@ import NextAuth, { NextAuthOptions, AuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 
-export const authOptions: AuthOptions = {
+export const authOptions: NextAuthOptions = {
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID ?? '',
